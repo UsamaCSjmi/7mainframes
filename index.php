a2758f3c449d250508d68118b890fb9cbffae1a5
Usama Husain <nhusain34@gmail.com>
2024-02-05 22:03:43 +0530

    Login Page Ready

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="robots" content="noindex,nofollow">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style.css">
+    <title>7 Main Frames</title>
+</head>
+<body>
+    <div class="form-wrapper">
+        <form action="">
+            <h1>Welcome to 7 Main Frames</h1>
+            <p>Write to us, we will reach you out.</p>
+            <div class="form-row">
+                <div class="form-group">
+                    <label for="name">Name:*</label>
+                    <input type="text" required>
+                </div>
+                <div class="form-group">
+                    <label for="name">Email:*</label>
+                    <input type="email" required>
+                </div>
+            </div>
+            <div class="form-row">
+                <div class="form-group">
+                    <label for="name">Phone:*</label>
+                    <input type="number" required>
+                </div>
+                <div class="form-group">
+                    <label for="name">City:</label>
+                    <input type="text">
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="message">Message:</label>
+                <textarea name="message" cols="30" rows="10"></textarea>
+            </div>
+        </form>
+    </div>
+</body>
+</html>
